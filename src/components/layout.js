6b805bba5257c047692c60017329377ecd3c44be
Reuben Berghan <reuben.berghan@gmail.com>
2019-01-12 15:13:22 +1300

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,47 @@
+import './layout.css'
+
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0
+          }}
+        >
+          {children}
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href='https://www.gatsbyjs.org'>Gatsby</a>
+          </footer>
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hello world</h1>
+  </Layout>
+)
+
+export default IndexPage
